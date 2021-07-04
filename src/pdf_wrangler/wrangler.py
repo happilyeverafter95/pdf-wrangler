5b4f6986ed27679c19f75dd2ef7915ac84380179
Mandy Gu <mandygu925@gmail.com>
2021-07-03 23:18:46 -0400

    minor changes

--- src/pdf_wrangler/wrangler.py
+++ src/pdf_wrangler/wrangler.py
@@ -1,7 +1,5 @@
-import datetime
 import base64
 import logging
-import zlib
 from io import BytesIO
 from typing import List, Dict, Optional
 
@@ -35,10 +33,10 @@ class PDFImage:
         self.__page_num = page_num
         try:
             channel = self._get_image_mode(image)
-            image_rawdata = zlib.decompress(image.stream.get_rawdata())
+            image_rawdata = image.stream.get_rawdata()
             self.image = Image.frombytes(channel, image.srcsize, image_rawdata)
         except Exception as e:
-            logger.warn(f'Unable to extract image {num_image} from page {page_num} due to {e}')
+            logger.warn(f'Unable to extract image {num_image} from page {page_num} due to "{e}"')
             self.image = None
 
     def __repr__(self) -> str:
@@ -63,7 +61,13 @@ class Page:
     def __init__(self, raw_page: LTPage, __page_num: int, filepath: str) -> None:
         self.__page_num = __page_num + 1
         self.filepath = filepath
-        self.text = self.extract_text(raw_page)
+
+        try:
+            self.text = raw_page.groups[0].get_text()
+        except:
+            # TODO: stricter exception handling
+            self.text = ''
+
         self.images = self.extract_images_from_page(raw_page)
 
     def __repr__(self) -> str:
@@ -72,12 +76,8 @@ class Page:
     def __str__(self) -> str:
         return f'PDF page {self.__page_num} for {self.filepath}'
 
-    def extract_text(self, page: LTPage) -> str:
-        try:
-            return page.groups[0].get_text()
-        except:
-            # TODO: stricter exception handling, logging error message
-            return ''
+    def get_text(self) -> str:
+        return self.text
 
     def extract_raw_images(self, page: LTPage, images: List[LTImage]) -> List[LTImage]:
         for lt_obj in page:
@@ -96,11 +96,11 @@ class Document:
     def __init__(self, filepath: str, password: Optional[str] = None) -> None:
         self.filepath = filepath
         self.buffered = BytesIO()
-        self.buffered.write(base64.b64decode(self.read_file()))
+        self.buffered.write(base64.b64decode(self._read_file()))
         self.password = password
         self.pages = []
-        self.extract_pdf_pages()
-        self.metadata = self.extract_pdf_metadata()
+        self._extract_pdf_pages()
+        self.metadata = self._extract_pdf_metadata()
 
     def __repr__(self) -> str:
         return str(self)
@@ -108,11 +108,11 @@ class Document:
     def __str__(self) -> str:
         return f'PDF from {self.filepath}'
 
-    def read_file(self) -> None:
+    def _read_file(self) -> None:
         with open(self.filepath, 'rb') as f:
             return base64.b64encode(f.read())
 
-    def extract_pdf_pages(self) -> None:
+    def _extract_pdf_pages(self) -> None:
         self.buffered.seek(0)
         rsrcmgr = PDFResourceManager(caching=False)
         device = PDFPageAggregator(rsrcmgr, laparams=LAParams())
@@ -129,14 +129,11 @@ class Document:
                 self.pages.append(device.get_result())
         except Exception as e:
             # TODO: make exception handling more strict
-            raise Exception(f'Unable to parse PDF pages due to {e}')
+            raise Exception(f'Unable to parse PDF pages due to "{e}"')
         device.close()
-        self.parse_pdf_pages()    
-
-    def parse_pdf_pages(self) -> None:
         self.pages = [Page(page, i, self.filepath) for i, page in enumerate(self.pages)]
 
-    def extract_pdf_metadata(self) -> Dict[str, str]:
+    def _extract_pdf_metadata(self) -> Dict[str, str]:
         parser = PDFParser(self.buffered)
         try:
             doc = PDFMinerDocument(parser)
@@ -144,3 +141,9 @@ class Document:
             return {}
         raw_metadata = doc.info[0] if len(doc.info) == 1 else {}
         return process_metadata(raw_metadata)
+
+    def get_text(self) -> str:
+        return '\n\n'.join([x.text for x in self.pages])
+
+    def get_metadata(self) -> dict:
+        return self.metadata
