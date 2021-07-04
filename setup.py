23bb4600da85cf34fd7d5854d83652582a0b3478
Mandy Gu <mandygu925@gmail.com>
2021-07-04 17:40:21 -0400

    add some keywords

--- setup.py
+++ setup.py
@@ -26,6 +26,7 @@ setuptools.setup(
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
     ],
+    keywords=['pdf parser', 'text mining'],
     package_dir={'': 'src'},
     packages=setuptools.find_packages(where='src'),
     python_requires='>=3.6',
